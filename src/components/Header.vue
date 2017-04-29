91abfb7637ad15bbf1753228abbe53ae834c3ec8
AnnaSu <newstory0113@gmail.com>
2017-04-29 14:01:53 +0800

    create Header style

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,43 @@
 <template>
-    <div class="header">
-    </div>  
+    <header class="header">
+        <img class="header-icon header__setting" src="../assets/icon_setting.svg" alt="">
+        <div class="header__location">
+            <img class="header-icon" src="../assets/icon_location.svg" alt="">
+            <p class="header__location__name">新北市 白沙灣</p>
+        </div>
+        <img class="header-icon header__pin" src="../assets/icon_pin.svg" alt="">
+    </header>  
 </template>
 
 <script>
     
 </script>
 
-<style lang="css">
-    
+<style lang="scss">
+    .header {
+        padding: 10px;
+        text-align: center;
+
+        .header-icon {
+            width: 20px;
+            height: 20px;
+            display: inline-block;
+            vertical-align: bottom;
+        }
+
+        .header__setting {
+            position: fixed;
+            left: 10px;
+        }
+
+        .header__pin {
+            position: fixed;
+            right: 10px;
+        }
+
+        .header__location, .header__location__name {
+            display: inline-block;
+            text-align: center;
+        }
+    }
 </style>
