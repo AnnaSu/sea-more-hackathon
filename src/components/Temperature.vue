0e3a41b673df8a7040386b83b9236933bfa0bd1e
AnnaSu <newstory0113@gmail.com>
2017-04-29 16:26:32 +0800

    add temperature info

--- src/components/Temperature.vue
+++ src/components/Temperature.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h3> Temperature </h3>
+    <div class="temperature">
+        <div class="temperature__info">
+            <img class="temperature__info__icon" src="../assets/icon_sun.svg" alt="">
+            <p class="temperature__info__number">氣溫 32°</p>
+        </div>
     </div>
 </template>
 
@@ -8,6 +11,21 @@
 
 </script>
 
-<style lang="css">
-
+<style lang="scss">
+    .temperature {
+        margin-top: 50px;
+        text-align: center;
+    }
+    .temperature__info__icon {
+        width: 20px;
+        height: 20px;
+        display: inline-block;
+        vertical-align: bottom;
+        cursor: pointer;
+    }
+    .temperature__info, .temperature__info__number {
+        display: inline-block;
+        cursor: pointer;
+        text-align: center;
+    }
 </style>
