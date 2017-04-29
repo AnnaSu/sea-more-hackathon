dd9a8c6f2cf3b0619ba5d0e435c501e219714205
AnnaSu <newstory0113@gmail.com>
2017-04-30 03:15:00 +0800

    add background color for App

--- src/App.vue
+++ src/App.vue
@@ -1,88 +1,41 @@
 <!-- The ref attr used to find the swiper instance -->
-
 <template>
-<div class="sea-more sea-more--sun">
-  <md-card>
-    <md-card-actions v-md-ink-ripple>
-      <div class="md-subhead">
+  <div class="sea-more sea-more--sun">
+  <div class="md-subhead">
         <Header />
       </div>
-      <md-button class="md-icon-button"
-                 target="_blank"
-                 href="https://github.com/surmon-china/vue-awesome-swiper/blob/master/examples/01-default.vue">
-      </md-button>
-    </md-card-actions>
-    <md-card-media>
-      <!-- swiper -->
-      <swiper :options="swiperOption">
-        <swiper-slide><Pages pageIndex="0"/></swiper-slide>
-        <swiper-slide><Pages pageIndex="1"/></swiper-slide>
-        <swiper-slide><Pages pageIndex="2"/></swiper-slide>
-        <div class="swiper-pagination" slot="pagination"></div>
-            <div class="space-man">
-                <div class="chat">太陽很大喔～建議中午時段不要曝曬超過20分鐘！</div>
-                <img src="/src/assets/spaceman.svg">
-            </div>
-      </swiper>
-    </md-card-media>
-  </md-card>
-</div>
+      <swiper :options="swiperOption" ref="mySwiper">
+      <!-- slides -->
+      <swiper-slide><Pages pageIndex="0"/></swiper-slide>
+      <swiper-slide><Pages pageIndex="1"/></swiper-slide>
+      <swiper-slide><Pages pageIndex="2"/></swiper-slide>
+      <!-- Optional controls -->
+      <div class="space-man">
+          <div class="chat">太陽很大喔～建議中午時段不要曝曬超過20分鐘！</div>
+          <img src="/src/assets/spaceman.svg">
+      </div>
+    </swiper>
+  </div>  
 </template>
-
-<script>
-  // swiper options example:
-  export default {
-    name: 'carrousel',
-    data() {
-      return {
-        swiperOption: {
-          // NotNextTick is a component's own property, and if notNextTick is set to true, the component will not instantiate the swiper through NextTick, which means you can get the swiper object the first time (if you need to use the get swiper object to do what Things, then this property must be true)
-          notNextTick: true,
-          direction : 'vertical',
-          grabCursor : true,
-          setWrapperSize :true,
-          autoHeight: true,
-          pagination : '.swiper-pagination',
-          paginationClickable :true,
-          prevButton:'.swiper-button-prev',
-          nextButton:'.swiper-button-next',
-          scrollbar:'.swiper-scrollbar',
-          mousewheelControl : true,
-          observeParents:true,
-          // if you need use plugins in the swiper, you can config in here like this
-          debugger: true,
-          // swiper callbacks
-          onTransitionStart(swiper){
-            console.log(swiper)
-          },
-          // more Swiper configs and callbacks...
-          // ...
-        }
-      }
-    },
-    // you can find current swiper instance object like this, while the notNextTick property value must be true
-    computed: {
-      swiper() {
-        return this.$refs.mySwiper.swiper
-      }
-    },
-    mounted() {
-      // you can use current swiper instance object to do something(swiper methods)
-      console.log('this is current swiper instance object', this.swiper)
-      this.swiper.slideTo(3, 1000, false)
-    }
-  }
-</script>
 
 <script>
 import { swiper, swiperSlide } from 'vue-awesome-swiper';
 import Header from './components/Header.vue';
 import Pages from './components/Pages.vue';
 export default {
-  name: 'app',
-  data () {
+  data() {
     return {
-      msg: 'hello'
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        onTransitionStart(swiper){
+          var bgColor = ['sun', 'rain', 'dusk'],
+          app = document.querySelector('.sea-more');
+          app.className = 'sea-more';
+          app.classList.add('sea-more--' + bgColor[swiper.activeIndex]);
+          console.log(swiper.activeIndex)
+        }
+      }
     }
   },
   components: {
